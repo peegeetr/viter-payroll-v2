f365af40d8eb065ef0274263afad69ab00ce6fb2
Cyrhina <cyrenemlumabas@gmail.com>
2023-02-07 13:08:00 +0800

    update ni cy

--- src/component/helpers/functions-general.jsx
+++ src/component/helpers/functions-general.jsx
@@ -9,10 +9,10 @@ export const hrisdevBaseUrl = "https://hris.frontlinebusiness.com.ph/dev-app";
 export const hrisdevNavUrl = "/dev-app";
 
 // Online URL dev payroll
-// export const devApiUrl = "https://payroll-app.frontlinebusiness.com.ph/rest";
-// export const devBaseImgUrl = "https://payroll-app.frontlinebusiness.com.ph/img";
-// export const devBaseUrl = "https://payroll-app.frontlinebusiness.com.ph";
-// export const devNavUrl = "";
+export const devApiUrl = "https://payroll-app.frontlinebusiness.com.ph/rest";
+export const devBaseImgUrl = "https://payroll-app.frontlinebusiness.com.ph/img";
+export const devBaseUrl = "https://payroll-app.frontlinebusiness.com.ph";
+export const devNavUrl = "";
 
 // Local URL dev
 // export const devApiUrl = "http://localhost/react-vite/viter-hris-v3/rest";
@@ -23,10 +23,10 @@ export const hrisdevNavUrl = "/dev-app";
 // export const devNavUrl = "/dev-app";
 
 // Local URL dev
-export const devApiUrl = "http://localhost/viter-payroll-v2/rest";
-export const devBaseUrl = "http://localhost/viter-payroll-v2/public";
-export const devBaseImgUrl = "http://localhost/viter-payroll-v2/public/img";
-export const devNavUrl = "";
+// export const devApiUrl = "http://localhost/viter-payroll-v2/rest";
+// export const devBaseUrl = "http://localhost/viter-payroll-v2/public";
+// export const devBaseImgUrl = "http://localhost/viter-payroll-v2/public/img";
+// export const devNavUrl = "";
 
 // // cy url
 // export const devApiUrl = "http://localhost/projects/viter-payroll-v2/rest";

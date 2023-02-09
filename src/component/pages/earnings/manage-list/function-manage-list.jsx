f248c30782fdf6fb6d7c17b919e3a41917b76b66
peegeetr <patrick.reyes@frontlinebusiness.com.ph>
2023-02-10 07:50:23 +0800

    updating version

--- src/component/pages/earnings/manage-list/function-manage-list.jsx
+++ src/component/pages/earnings/manage-list/function-manage-list.jsx
@@ -14,31 +14,34 @@ export const getDateLength = (
   const endYear = end.getFullYear() - payStart.getFullYear();
   const endDays = end.getDate() - payStart.getDate();
   const endMonths = end.getMonth() - payStart.getMonth();
-  const endDate = endYear * 365 + endMonths * 30.417 + endDays;
+  const endDateLength = endYear * 365 + endMonths * 30.417 + endDays;
 
   //start date
   const startYear = payEendD.getFullYear() - start.getFullYear();
   const startDays = payEendD.getDate() - start.getDate();
   const startMonths = payEendD.getMonth() - start.getMonth();
-  const startDate = startYear * 365 + startMonths * 30.417 + startDays;
+  const startDateLength = startYear * 365 + startMonths * 30.417 + startDays;
 
   //pay date
   const payYear = payEendD.getFullYear() - payStart.getFullYear();
   const payDays = payEendD.getDate() - payStart.getDate();
   const payMonths = payEendD.getMonth() - payStart.getMonth();
-  const PayDate = payYear * 365 + payMonths * 30.417 + payDays;
+  const PayDateLength = payYear * 365 + payMonths * 30.417 + payDays;
 
   // return `${startDate} - ${endDate} = ${PayDate}`;
   //result
   if (inputStartDate === "n/a" && inputendDate === "n/a") {
-    return `true1 ${startDate} - ${endDate} = ${PayDate}`;
-  } else if (endDate < "0" || startDate < "0") {
-    return `false1 ${startDate} - ${endDate} = ${PayDate}`;
-  } else if (endDate <= PayDate && startDate <= PayDate) {
-    return `true2 ${startDate} - ${endDate} = ${PayDate}`;
-  } else if (endDate === "0" && startDate === "0") {
-    return `true3 ${startDate} - ${endDate} = ${PayDate}`;
+    return `true1 ${startDateLength} - ${endDateLength} = ${PayDateLength}`;
+  } else if (endDateLength < "0" || startDateLength < "0") {
+    return `false1 ${startDateLength} - ${endDateLength} = ${PayDateLength}`;
+  } else if (
+    endDateLength <= PayDateLength &&
+    startDateLength <= PayDateLength
+  ) {
+    return `true2 ${startDateLength} - ${endDateLength} = ${PayDateLength}`;
+  } else if (endDateLength === "0" && startDateLength === "0") {
+    return `true3 ${startDateLength} - ${endDateLength} = ${PayDateLength}`;
   } else {
-    return `false2 ${startDate} - ${endDate} = ${PayDate}`;
+    return `false2 ${startDateLength} - ${endDateLength} = ${PayDateLength}`;
   }
 };

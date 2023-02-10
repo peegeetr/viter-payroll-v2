291c158281e2b9496cf435eebce2f89cdf58c890
Cyrhina <cyrenemlumabas@gmail.com>
2023-02-10 09:32:27 +0800

    update ni cy

--- src/component/pages/earnings/manage-list/function-manage-list.jsx
+++ src/component/pages/earnings/manage-list/function-manage-list.jsx
@@ -1,57 +1,11 @@
 import { setError, setMessage } from "../../../../store/StoreAction";
 
 // get Date Length
-export const getDateLength = (
-  inputStartDate,
-  inputendDate,
-  payStartDate,
-  payEndDate
-) => {
-  const start = new Date(inputStartDate);
-  const end = new Date(inputendDate);
-  const payStart = new Date(payStartDate);
-  const payEendD = new Date(payEndDate);
-
-  //end date
-  const endYear = end.getFullYear() - payStart.getFullYear();
-  const endDays = end.getDate() - payStart.getDate();
-  const endMonths = end.getMonth() - payStart.getMonth();
-  const endDateLength = endYear * 365 + endMonths * 30.417 + endDays;
-
-  //start date
-  const startYear = payEendD.getFullYear() - start.getFullYear();
-  const startDays = payEendD.getDate() - start.getDate();
-  const startMonths = payEendD.getMonth() - start.getMonth();
-  const startDateLength = startYear * 365 + startMonths * 30.417 + startDays;
-
-  //pay date
-  const payYear = payEendD.getFullYear() - payStart.getFullYear();
-  const payDays = payEendD.getDate() - payStart.getDate();
-  const payMonths = payEendD.getMonth() - payStart.getMonth();
-  const PayDateLength = payYear * 365 + payMonths * 30.417 + payDays;
-
-  // return `${startDate} - ${endDate} = ${PayDate}`;
-  //result
-  if (inputStartDate === "n/a" && inputendDate === "n/a") {
-    return `true1 ${startDateLength} - ${endDateLength} = ${PayDateLength}`;
-  } else if (endDateLength < "0" || startDateLength < "0") {
-    return `false1 ${startDateLength} - ${endDateLength} = ${PayDateLength}`;
-  } else if (
-    endDateLength <= PayDateLength &&
-    startDateLength <= PayDateLength
-  ) {
-    return `true2 ${startDateLength} - ${endDateLength} = ${PayDateLength}`;
-  } else if (endDateLength === "0" && startDateLength === "0") {
-    return `true3 ${startDateLength} - ${endDateLength} = ${PayDateLength}`;
-  } else {
-    return `false2 ${startDateLength} - ${endDateLength} = ${PayDateLength}`;
-  }
-};
-// get Date Length
 export const validatePayPeriod = (values, payrollDraft, dispatch) => {
+  let val = false;
   if (
-    new Date(values.earnings_start_pay_date) >
-      new Date(payrollDraft[0].payroll_start_date) &&
+    new Date(values.earnings_start_pay_date) <
+      new Date(payrollDraft[0].payroll_start_date) ||
     new Date(values.earnings_end_pay_date) >
       new Date(payrollDraft[0].payroll_end_date)
   ) {
@@ -59,6 +13,7 @@ export const validatePayPeriod = (values, payrollDraft, dispatch) => {
     dispatch(
       setMessage("Start date and end date is not avilable for pay date.")
     );
-    return "Start date and end date is not avilable for pay date.";
+    val = true;
   }
+  return val;
 };

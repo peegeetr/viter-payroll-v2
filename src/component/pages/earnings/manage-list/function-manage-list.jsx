f47b0f7e83c2ce0d33167df90b76cddb5f4a8fa5
peegeetr <patrick.reyes@frontlinebusiness.com.ph>
2023-02-14 12:31:52 +0800

    updating leave amount

--- src/component/pages/earnings/manage-list/function-manage-list.jsx
+++ src/component/pages/earnings/manage-list/function-manage-list.jsx
@@ -32,8 +32,7 @@ export const computeLeave = (leaveData, employee) => {
       if (Number(lItem.leave_list_employee_id) === Number(eItem.employee_aid)) {
         list.push({
           name: `${eItem.employee_lname}, ${eItem.employee_fname}`,
-          // amount: Number(lItem.leave_list_days) * 500,
-          amount: Number(1) * 500,
+          amount: Number(lItem.leave_list_days) * 500,
           employeId: eItem.employee_aid,
         });
       }

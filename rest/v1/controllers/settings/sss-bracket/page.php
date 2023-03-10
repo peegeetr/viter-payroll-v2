335147e44be9ed5f974271b016db78ab5a0344cf
peegeetr <patrick.reyes@frontlinebusiness.com.ph>
2023-03-10 09:48:25 +0800

    updating payroll

--- rest/v1/controllers/settings/sss-bracket/page.php
+++ rest/v1/controllers/settings/sss-bracket/page.php
@@ -20,7 +20,7 @@ if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
         // get data
         // get task id from query string
         $sss_bracket->sss_bracket_start = $_GET['start'];
-        $sss_bracket->sss_bracket_total = 5;
+        $sss_bracket->sss_bracket_total = 30;
         //check to see if task id in query string is not empty and is number, if not return json error
         checkLimitId($sss_bracket->sss_bracket_start, $sss_bracket->sss_bracket_total);
         $query = checkReadLimit($sss_bracket);

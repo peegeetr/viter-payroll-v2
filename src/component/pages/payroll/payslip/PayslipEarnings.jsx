bae96e6463cb51a97c4dde55498eb68826fc370e
peegeetr <patrick.reyes@frontlinebusiness.com.ph>
2023-03-16 17:51:19 +0800

    updating slip:

--- src/component/pages/payroll/payslip/PayslipEarnings.jsx
+++ src/component/pages/payroll/payslip/PayslipEarnings.jsx
@@ -7,73 +7,88 @@ import {
   otherBenefitsEarningsId,
   wagesEarningsId,
 } from "../../../helpers/functions-payitemId";
+import TableSpinner from "../../../partials/spinners/TableSpinner";
 const PayslipEarnings = ({ paytypeId, empid, payrollid, hourRate, days }) => {
   // use if not loadmore button undertime
-  const { data: earnings } = useQueryData(
+  const { data: earnings, isLoading } = useQueryData(
     `${devApiUrl}/v1/payslip/earnings/${paytypeId}/${empid}/${payrollid}`, // endpoint
     "get", // method
     `earnings-${paytypeId}` // key
   );
-  let totalAmount = 0;
-  let basicPay = hourRate * (days - 1) * 8;
-  console.log(earnings);
+  let numberOfHolidays = earnings?.data.length > 0 ? earnings?.data.length : 0;
+  let basicPay = hourRate * (days - numberOfHolidays) * 8;
+  let basicHrs = days - numberOfHolidays;
+  let totalAmount = basicPay;
   // setTotalEarnings(totalAmount);
+
   return (
     <>
-      {paytypeId === wagesEarningsId && (
-        <>
-          <tr className="font-semibold bg-gray-100 hover:bg-gray-100 uppercase">
-            <td className="w-[30rem]">WAGES</td>
-            <td className="w-[10rem]">hours</td>
-            <td>rate</td>
-            <td>total</td>
-          </tr>
-          <tr className="hover:bg-transparent">
-            <td className="w-[20rem]">Basic Pay (Deminimis inclusive)</td>
-            <td className="w-[10rem]">80</td>
-            <td>{hourRate}</td>
-            <td>{numberWithCommas(basicPay.toFixed(2))}</td>
-          </tr>
-        </>
-      )}
-
-      {paytypeId === otherBenefitsEarningsId && earnings?.data.length > 0 && (
-        <>
-          <tr className="hover:bg-white">
-            <td colSpan={4}></td>
-          </tr>
-          <tr className="font-semibold bg-gray-100 hover:bg-gray-100">
-            <td colSpan={4} className="w-[20rem] uppercase">
-              {earnings?.data[0].paytype_name}
-            </td>
-          </tr>
-        </>
-      )}
-      {earnings?.data.map((item, key) => {
-        totalAmount += Number(item.earnings_amount);
-        // setTotalEarnings((prevState) => prevState + totalAmount);
-        return (
-          <tr key={key} className="hover:bg-transparent">
-            <td className="w-[20rem]">
-              {item.payitem_name} {item.earnings_details}
-            </td>
-            <td className="w-[10rem]">8</td>
-            <td>
-              {(hourRate * (Number(item.earnings_holidays_rate) / 100)).toFixed(
-                4
-              )}
-            </td>
-            <td>{numberWithCommas(item.earnings_amount)}</td>
-          </tr>
-        );
-      })}
-      {earnings?.data.length > 0 && (
-        <tr className="font-semibold bg-gray-100 hover:bg-gray-100">
-          <td colSpan={3} className="uppercase text-right xs:pr-16">
-            Total {earnings?.data[0].paytype_name}
+      {isLoading ? (
+        <tr className="text-center ">
+          <td colSpan="100%" className="p-10">
+            {/* <TableSpinner /> */}
+            Loading...
           </td>
-          <td>{numberWithCommas(totalAmount.toFixed(2))}</td>
         </tr>
+      ) : (
+        <>
+          {paytypeId === wagesEarningsId && (
+            <>
+              <tr className="font-semibold bg-gray-100 hover:bg-gray-100 uppercase">
+                <td className="w-[30rem]">WAGES</td>
+                <td className="w-[10rem]">hours</td>
+                <td>rate</td>
+                <td>total</td>
+              </tr>
+              <tr className="hover:bg-transparent">
+                <td className="w-[20rem]">Basic Pay (Deminimis inclusive)</td>
+                <td className="w-[10rem]">{basicHrs * 8}</td>
+                <td>{hourRate}</td>
+                <td>{numberWithCommas(basicPay.toFixed(2))}</td>
+              </tr>
+            </>
+          )}
+
+          {paytypeId === otherBenefitsEarningsId &&
+            earnings?.data.length > 0 && (
+              <>
+                <tr className="hover:bg-white">
+                  <td colSpan={4}></td>
+                </tr>
+                <tr className="font-semibold bg-gray-100 hover:bg-gray-100">
+                  <td colSpan={4} className="w-[20rem] uppercase">
+                    {earnings?.data[0].paytype_name}
+                  </td>
+                </tr>
+              </>
+            )}
+          {earnings?.data.map((item, key) => {
+            totalAmount += Number(item.earnings_amount);
+            return (
+              <tr key={key} className="hover:bg-transparent">
+                <td className="w-[20rem]">
+                  {item.payitem_name} {item.earnings_details}
+                </td>
+                <td className="w-[10rem]">{numberOfHolidays * 8}</td>
+                <td>
+                  {(
+                    hourRate *
+                    (Number(item.earnings_holidays_rate) / 100)
+                  ).toFixed(4)}
+                </td>
+                <td>{numberWithCommas(item.earnings_amount)}</td>
+              </tr>
+            );
+          })}
+          {earnings?.data.length > 0 && (
+            <tr className="font-semibold bg-gray-100 hover:bg-gray-100">
+              <td colSpan={3} className="uppercase text-right xs:pr-16">
+                Total {earnings?.data[0].paytype_name}
+              </td>
+              <td>{numberWithCommas(totalAmount.toFixed(2))}</td>
+            </tr>
+          )}
+        </>
       )}
     </>
   );
--- src/component/pages/payroll/payslip/PayslipMandatoryDeduc.jsx
+++ src/component/pages/payroll/payslip/PayslipMandatoryDeduc.jsx
@@ -10,7 +10,7 @@ import {
 import { totalSum } from "./functionPayslip";
 const PayslipMandatoryDeduc = ({ paytypeId, payslip, empid, payrollid }) => {
   // use if not loadmore button undertime
-  const { data: deductions } = useQueryData(
+  const { data: deductions, isLoading } = useQueryData(
     `${devApiUrl}/v1/payslip/deductions/${paytypeId}/${empid}/${payrollid}`, // endpoint
     "get", // method
     `deductions-${paytypeId}` // key
@@ -18,61 +18,57 @@ const PayslipMandatoryDeduc = ({ paytypeId, payslip, empid, payrollid }) => {
 
   return (
     <>
-      {payslip?.data.length > 0 && (
+      {isLoading ? (
+        <tr className="text-center ">
+          <td colSpan="100%" className="p-10">
+            {/* <TableSpinner /> */}
+            Loading...
+          </td>
+        </tr>
+      ) : (
         <>
-          <tr className="font-semibold bg-gray-100 hover:bg-gray-100">
-            <td className="w-[20rem] uppercase">Mandatory deduction</td>
-            <td className="w-[20rem] uppercase">EE</td>
-            <td colSpan={2} className="w-[20rem] uppercase">
-              ER
-            </td>
-          </tr>
-          <tr className="hover:bg-transparent">
-            <td className="w-[20rem]"> SSS</td>
-            <td className="w-[10rem]">
-              {payslip?.data[0].payroll_list_sss_ee}
-            </td>
-            <td>{payslip?.data[0].payroll_list_sss_er} </td>
-            <td>
-              {totalSum(
-                payslip?.data[0].payroll_list_sss_ee,
-                payslip?.data[0].payroll_list_sss_er
-              )}
-            </td>
-            {/* <td>{numberWithCommas(Number(sssTotal).toFixed(2))} </td> */}
-          </tr>
-          <tr className="hover:bg-transparent">
-            <td className="w-[20rem]">Pag-ibig</td>
-            <td className="w-[10rem]">
-              {payslip?.data[0].payroll_list_pagibig_ee}
-            </td>
-            <td>{payslip?.data[0].payroll_list_pagibig_er}</td>
-            <td>
-              {totalSum(
-                payslip?.data[0].payroll_list_pagibig_ee,
-                payslip?.data[0].payroll_list_pagibig_er
-              )}
-            </td>
-          </tr>
-          <tr className="hover:bg-transparent">
-            <td className="w-[20rem]">Philhealth</td>
-            <td className="w-[10rem]">
-              {payslip?.data[0].payroll_list_philhealth_ee}
-            </td>
-            <td>{payslip?.data[0].payroll_list_philhealth_er}</td>
-            <td>
-              {totalSum(
-                payslip?.data[0].payroll_list_philhealth_ee,
-                payslip?.data[0].payroll_list_philhealth_er
-              )}
-            </td>
-          </tr>
-          <tr className="font-semibold bg-gray-100 hover:bg-gray-100">
-            <td colSpan={3} className="uppercase text-right xs:pr-16">
-              Total Mandatory deduction
-            </td>
-            <td>0.00</td>
-          </tr>
+          {payslip?.data.length > 0 && (
+            <>
+              <tr className="font-semibold bg-gray-100 hover:bg-gray-100">
+                <td className="w-[20rem] uppercase">Mandatory deduction</td>
+                <td className="w-[20rem] uppercase">EE</td>
+                <td colSpan={2} className="w-[20rem] uppercase">
+                  ER
+                </td>
+              </tr>
+              <tr className="hover:bg-transparent">
+                <td className="w-[20rem]"> SSS</td>
+                <td className="w-[10rem]">
+                  {payslip?.data[0].payroll_list_sss_ee}
+                </td>
+                <td>{payslip?.data[0].payroll_list_sss_er} </td>
+                <td>{payslip?.data[0].payroll_list_sss_ee}</td>
+                {/* <td>{numberWithCommas(Number(sssTotal).toFixed(2))} </td> */}
+              </tr>
+              <tr className="hover:bg-transparent">
+                <td className="w-[20rem]">Pag-ibig</td>
+                <td className="w-[10rem]">
+                  {payslip?.data[0].payroll_list_pagibig_ee}
+                </td>
+                <td>{payslip?.data[0].payroll_list_pagibig_er}</td>
+                <td>{payslip?.data[0].payroll_list_pagibig_ee}</td>
+              </tr>
+              <tr className="hover:bg-transparent">
+                <td className="w-[20rem]">Philhealth</td>
+                <td className="w-[10rem]">
+                  {payslip?.data[0].payroll_list_philhealth_ee}
+                </td>
+                <td>{payslip?.data[0].payroll_list_philhealth_er}</td>
+                <td>{payslip?.data[0].payroll_list_philhealth_ee}</td>
+              </tr>
+              <tr className="font-semibold bg-gray-100 hover:bg-gray-100">
+                <td colSpan={3} className="uppercase text-right xs:pr-16">
+                  Total Mandatory deduction
+                </td>
+                <td>0.00</td>
+              </tr>
+            </>
+          )}
         </>
       )}
     </>

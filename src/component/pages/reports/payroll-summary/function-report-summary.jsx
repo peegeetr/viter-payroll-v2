e64b6d98cad38cabd1d98257ee96c15005dd1926
monmonplaza <monmon.plaza@gmail.com>
2023-03-17 09:19:14 +0800

    fix sticky table header

--- src/component/pages/reports/payroll-summary/function-report-summary.jsx
+++ src/component/pages/reports/payroll-summary/function-report-summary.jsx
@@ -46,3 +46,20 @@ export const getErningsHolidayRate = (earning, item) => {
   console.log("types", list, earning);
   return list;
 };
+
+/* table {
+    position: relative
+}
+
+1st tr {
+  position: sticky
+  top: 0
+}
+
+2nd tr {
+  position: sticky
+  top: 35px
+}
+
+
+*/

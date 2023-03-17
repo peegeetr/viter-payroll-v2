78b99c5115877b48a8cc609f3dc9f6bf265338d5
peegeetr <patrick.reyes@frontlinebusiness.com.ph>
2023-03-17 17:06:17 +0800

    updating slip

--- src/component/pages/payroll/payslip/PayslipMandatoryDeduc.jsx
+++ src/component/pages/payroll/payslip/PayslipMandatoryDeduc.jsx
@@ -15,64 +15,53 @@ const PayslipMandatoryDeduc = ({ paytypeId, payslip, empid, payrollid }) => {
     "get", // method
     `deductions-${paytypeId}` // key
   );
-
+  // console.log(deductions);
   return (
     <>
-      {isLoading ? (
-        <tr className="text-center ">
-          <td colSpan="100%" className="p-10">
-            {/* <TableSpinner /> */}
-            Loading...
-          </td>
-        </tr>
-      ) : (
+      {payslip?.data.length > 0 && (
         <>
-          {payslip?.data.length > 0 && (
-            <>
-              <tr className="font-semibold bg-gray-100 hover:bg-gray-100">
-                <td className="w-[20rem] uppercase">Mandatory deduction</td>
-                <td className="w-[20rem] uppercase">EE</td>
-                <td colSpan={2} className="w-[20rem] uppercase">
-                  ER
-                </td>
-              </tr>
-              <tr className="hover:bg-transparent">
-                <td className="w-[20rem]"> SSS</td>
-                <td className="w-[10rem]">
-                  {payslip?.data[0].payroll_list_sss_ee}
-                </td>
-                <td>{payslip?.data[0].payroll_list_sss_er} </td>
-                <td>{payslip?.data[0].payroll_list_sss_ee}</td>
-                {/* <td>{numberWithCommas(Number(sssTotal).toFixed(2))} </td> */}
-              </tr>
-              <tr className="hover:bg-transparent">
-                <td className="w-[20rem]">Pag-ibig</td>
-                <td className="w-[10rem]">
-                  {payslip?.data[0].payroll_list_pagibig_ee}
-                </td>
-                <td>{payslip?.data[0].payroll_list_pagibig_er}</td>
-                <td>{payslip?.data[0].payroll_list_pagibig_ee}</td>
-              </tr>
-              <tr className="hover:bg-transparent">
-                <td className="w-[20rem]">Philhealth</td>
-                <td className="w-[10rem]">
-                  {payslip?.data[0].payroll_list_philhealth_ee}
-                </td>
-                <td>{payslip?.data[0].payroll_list_philhealth_er}</td>
-                <td>{payslip?.data[0].payroll_list_philhealth_ee}</td>
-              </tr>
-              <tr className="font-semibold bg-gray-100 hover:bg-gray-100">
-                <td colSpan={3} className="uppercase text-right xs:pr-16">
-                  Total Mandatory deduction
-                </td>
-                <td>
-                  {numberWithCommas(
-                    Number(payslip?.data[0].payroll_list_madatory_ee).toFixed(2)
-                  )}
-                </td>
-              </tr>
-            </>
-          )}
+          <tr className="font-semibold bg-gray-100 hover:bg-gray-100">
+            <td className="w-[20rem] uppercase">Mandatory deduction</td>
+            <td className="w-[20rem] uppercase">EE</td>
+            <td colSpan={2} className="w-[20rem] uppercase">
+              ER
+            </td>
+          </tr>
+          <tr className="hover:bg-transparent">
+            <td className="w-[20rem]"> SSS</td>
+            <td className="w-[10rem]">
+              {payslip?.data[0].payroll_list_sss_ee}
+            </td>
+            <td>{payslip?.data[0].payroll_list_sss_er} </td>
+            <td>{payslip?.data[0].payroll_list_sss_ee}</td>
+            {/* <td>{numberWithCommas(Number(sssTotal).toFixed(2))} </td> */}
+          </tr>
+          <tr className="hover:bg-transparent">
+            <td className="w-[20rem]">Pag-ibig</td>
+            <td className="w-[10rem]">
+              {payslip?.data[0].payroll_list_pagibig_ee}
+            </td>
+            <td>{payslip?.data[0].payroll_list_pagibig_er}</td>
+            <td>{payslip?.data[0].payroll_list_pagibig_ee}</td>
+          </tr>
+          <tr className="hover:bg-transparent">
+            <td className="w-[20rem]">Philhealth</td>
+            <td className="w-[10rem]">
+              {payslip?.data[0].payroll_list_philhealth_ee}
+            </td>
+            <td>{payslip?.data[0].payroll_list_philhealth_er}</td>
+            <td>{payslip?.data[0].payroll_list_philhealth_ee}</td>
+          </tr>
+          <tr className="font-semibold bg-gray-100 hover:bg-gray-100">
+            <td colSpan={3} className="uppercase text-right xs:pr-16">
+              Total Mandatory deduction
+            </td>
+            <td>
+              {numberWithCommas(
+                Number(payslip?.data[0].payroll_list_madatory_ee).toFixed(2)
+              )}
+            </td>
+          </tr>
         </>
       )}
     </>

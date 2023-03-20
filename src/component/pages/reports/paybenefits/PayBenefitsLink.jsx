5011dbbdd57891b3b4b653d2676ecc36929c76a4
Cyrhina <cyrenemlumabas@gmail.com>
2023-03-20 12:18:18 +0800

    update ni cy

--- src/component/pages/reports/paybenefits/PayBenefitsLink.jsx
+++ src/component/pages/reports/paybenefits/PayBenefitsLink.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BsFillCalculatorFill } from "react-icons/bs";
+import { FaHands } from "react-icons/fa";
 import { SlArrowRight } from "react-icons/sl";
 import { Link } from "react-router-dom";
 import { StoreContext } from "../../../../store/StoreContext";
@@ -13,7 +13,7 @@ const PayBenefitsLink = () => {
       <Link to={`${link}/reports/benefits`} className="w-full py-1">
         <div className="flex items-center">
           <span className="text-lg mr-4">
-            <BsFillCalculatorFill />
+            <FaHands />
           </span>
           <span className=" font-bold">Benefits</span>
         </div>

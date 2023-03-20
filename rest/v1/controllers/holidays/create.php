5796758a3acb35fc820e697db33c3987c8bd5dbf
peegeetr <patrick.reyes@frontlinebusiness.com.ph>
2023-03-20 10:14:30 +0800

    updating run

--- rest/v1/controllers/holidays/create.php
+++ rest/v1/controllers/holidays/create.php
@@ -22,7 +22,7 @@ $holidays->holidays_created = date("Y-m-d H:i:s");
 $holidays->holidays_datetime = date("Y-m-d H:i:s");
 
 isHolidayNameExist($holidays, $holidays->holidays_name);
-isHolidayDateExist($holidays, $holidays->holidays_date);
+// isHolidayDateExist($holidays, $holidays->holidays_date);
 
 // create
 $query = checkCreate($holidays);

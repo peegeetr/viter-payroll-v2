cb9f706cd3d7d4878dc17df50f2a3b1d9e02a6a1
peegeetr <patrick.reyes@frontlinebusiness.com.ph>
2023-04-27 13:43:30 +0800

    updating version

--- src/component/pages/holidays/list/ModalAddHolidays.jsx
+++ src/component/pages/holidays/list/ModalAddHolidays.jsx
@@ -142,12 +142,12 @@ const ModalAddHolidays = ({ item }) => {
                       {isObserved === "1" || isObserved === 1 ? (
                         <div className="flex items-center absolute right-[100px] mt-4 text-primary">
                           <FaCheck className="text-green-800" />
-                          <p className=" mb-0 ml-2">Will Observed</p>
+                          <p className=" mb-0 ml-2">Will Observe</p>
                         </div>
                       ) : isObserved === "0" || isObserved === 0 ? (
                         <div className="flex items-center absolute right-[86px] mt-4 text-primary">
                           <ImCross />
-                          <p className=" mb-0 ml-2">Will Not Observed</p>
+                          <p className=" mb-0 ml-2">Will not Observe</p>
                         </div>
                       ) : (
                         ""

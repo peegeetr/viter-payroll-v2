9f374bf0463889ce655188608634ed133da24970
Cyrhina <cyrenemlumabas@gmail.com>
2023-05-17 10:03:28 +0800

    update ni cy

--- src/component/pages/settings/system-mode/SystemModeList.jsx
+++ src/component/pages/settings/system-mode/SystemModeList.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { FaEdit, FaTrash } from "react-icons/fa";
-import { setIsAdd, setIsRestore } from "../../../../store/StoreAction";
+import { setIsConfirm, setIsRestore } from "../../../../store/StoreAction";
 import { StoreContext } from "../../../../store/StoreContext";
 import useQueryData from "../../../custom-hooks/useQueryData";
 import { devApiUrl } from "../../../helpers/functions-general";
-import ModalDeleteRestoreRq from "../../../partials/modals/ModalDeleteRestoreRq";
 import NoData from "../../../partials/NoData";
 import ServerError from "../../../partials/ServerError";
+import ModalConfirmRq from "../../../partials/modals/ModalConfirmRq";
+import ModalDeleteRestoreRq from "../../../partials/modals/ModalDeleteRestoreRq";
 import TableSpinner from "../../../partials/spinners/TableSpinner";
 
 const SystemModeList = () => {
@@ -17,28 +17,35 @@ const SystemModeList = () => {
   const [id, setId] = React.useState(null);
   const [isDel, setDel] = React.useState(false);
 
-  const is_open = false;
-  // const is_open =
-  //   result.length > 0 && result[0].enable_enrollment_is_enable === "1";
   // use if not loadmore button undertime
   const {
     isLoading,
     isFetching,
     error,
-    data: rate,
+    data: systemMode,
   } = useQueryData(
-    `${devApiUrl}/v1/payroll-type`, // endpoint
+    `${devApiUrl}/v1/system-mode`, // endpoint
     "get", // method
-    "rate" // key
+    "systemMode" // key
   );
-  console.log(rate);
-  const handleDelete = (item) => {
-    dispatch(setIsRestore(true));
-    setId(item.rates_aid);
+
+  // const is_open = false;
+
+  console.log(systemMode);
+
+  const handleTurnOn = (item) => {
+    dispatch(setIsConfirm(true));
+    setId(item.settings_system_mode_aid);
     setData(item);
-    setDel(true);
+    setDel(null);
   };
 
+  const handleTurnOff = (item) => {
+    dispatch(setIsRestore(true));
+    setId(item.settings_system_mode_aid);
+    setData(item);
+    setDel(null);
+  };
   let counter = 0;
 
   return (
@@ -49,11 +56,11 @@ const SystemModeList = () => {
             <tr>
               <th>#</th>
               <th>Name</th>
-              <th className="max-w-[7rem]">Actions</th>
+              <th className="max-w-[7rem] pr-4 text-right">Actions</th>
             </tr>
           </thead>
           <tbody>
-            {(isLoading || rate?.data.length === 0) && (
+            {(isLoading || systemMode?.data.length === 0) && (
               <tr className="text-center ">
                 <td colSpan="100%" className="p-10">
                   {isLoading && <TableSpinner />}
@@ -68,30 +75,39 @@ const SystemModeList = () => {
                 </td>
               </tr>
             )}
-            {rate?.data.map((item, key) => {
+            {systemMode?.data.map((item, key) => {
               counter++;
               return (
                 <tr key={key}>
                   <td>{counter}</td>
-                  <td>{item.payroll_type_name}</td>
+                  <td className="capitalize">
+                    {item.settings_system_mode_name}
+                  </td>
 
-                  <td>
-                    {/* <div className="switch__button">
-                      <div className="switch">
-                        <label>
-                          <input
-                            type="checkbox"
-                            checked={is_open === false ? false : true}
-                            // onChange={(e) => {
-                            //   result.length > 0
-                            //     ? dispatch(setIsConfirm(true))
-                            //     : dispatch(setIsAdd(true));
-                            // }}
-                          />
-                          <span className="slider"></span>
-                        </label>
+                  <td className="pr-4 ">
+                    <div className="flex justify-end ">
+                      <div
+                        className="hover:bg-white btn-action-table tooltip-action-table relative min-w-[40px] w-[40px] h-[20px] cursor-pointer ease-linear duration-[0.4s] rounded-full"
+                        data-tooltip={
+                          item.settings_system_mode_is_on === 0
+                            ? "Turn On"
+                            : "Turn Off"
+                        }
+                        onClick={() =>
+                          item.settings_system_mode_is_on === 0
+                            ? handleTurnOff(item)
+                            : handleTurnOn(item)
+                        }
+                      >
+                        <span
+                          className={
+                            item.settings_system_mode_is_on === 0
+                              ? "absolute top-0 bottom-0 right-0 left-[13.8px] cursor-pointer w-[25px] h-[19.5px] border border-solid border-primary rounded-full ease-linear duration-[0.4s] bg-primary opacity-[0.5]"
+                              : "absolute top-0 bottom-0 right-0 left-0 cursor-pointer w-[25px] h-[19.1px] border border-solid border-primary rounded-full ease-linear duration-[0.4s] bg-primary"
+                          }
+                        ></span>
                       </div>
-                    </div> */}
+                    </div>
                   </td>
                 </tr>
               );
@@ -100,14 +116,24 @@ const SystemModeList = () => {
         </table>
       </div>
 
+      {store.isConfirm && (
+        <ModalConfirmRq
+          id={id}
+          isDel={isDel}
+          mysqlApiArchive={`${devApiUrl}/v1/system-mode/active/${id}`}
+          msg={"Are you sure you want to turn off"}
+          item={`${dataItem.settings_system_mode_name}`}
+          arrKey="systemMode"
+        />
+      )}
       {store.isRestore && (
         <ModalDeleteRestoreRq
           id={id}
           isDel={isDel}
-          mysqlApiDelete={`${devApiUrl}/v1/rates/${id}`}
-          msg={"Are you sure you want to delete this rate"}
-          item={`${dataItem.rates_name}`}
-          arrKey="rate"
+          mysqlApiRestore={`${devApiUrl}/v1/system-mode/active/${id}`}
+          msg={"Are you sure you want to turn on"}
+          item={`${dataItem.settings_system_mode_name}`}
+          arrKey="systemMode"
         />
       )}
     </>

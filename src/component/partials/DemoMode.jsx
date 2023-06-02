a48d83399d71424bc5d03f814fd43c16e9c2e169
Cyrhina <cyrenemlumabas@gmail.com>
2023-06-02 14:06:00 +0800

    update ni cy

--- src/component/partials/DemoMode.jsx
+++ src/component/partials/DemoMode.jsx
@@ -2,8 +2,8 @@ import { FaInfoCircle } from "react-icons/fa";
 const DemoMode = () => {
   return (
     <>
-      <div className="absolute left-0 right-0 top-16">
-        <div className=" w-[10rem] mx-auto border-2 border-t-0 bg-lightWarning border-[#f09a02] flex justify-center items-center py-[2px] rounded-b-lg">
+      <div className="fixed left-0 right-0 top-8 z-30">
+        <div className=" w-[10rem] mx-auto border-2 bg-lightWarning border-[#f09a02] flex justify-center items-center py-[2px] rounded-t-lg">
           <FaInfoCircle className="text-[#f09a02] text-base" />
           <p className="text-base m-0 ml-2">Demo mode</p>
         </div>

495625cea19efb971849227af3be7d3cedbdc8c5
Cyrhina <cyrenemlumabas@gmail.com>
2023-06-22 08:44:24 +0800

    update ni cy

--- rest/v1/controllers/payroll/list/update-amount.php
+++ rest/v1/controllers/payroll/list/update-amount.php
@@ -31,26 +31,24 @@ if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
         checkId($payrollList->payroll_list_aid);
         $allPayrollList = $data["payrollList"];
         if (count($allPayrollList) > 0) {
-            for ($pl = 0; $pl < count($allPayrollList); $pl++) {
-                $payrollList->payroll_list_sss_er = $allPayrollList[$pl]["payroll_list_sss_er"];
-                $payrollList->payroll_list_sss_ee = $allPayrollList[$pl]["payroll_list_sss_ee"];
-                $payrollList->payroll_list_pagibig_er = $allPayrollList[$pl]["payroll_list_pagibig_er"];
-                $payrollList->payroll_list_pagibig_ee = $allPayrollList[$pl]["payroll_list_pagibig_ee"];
-                $payrollList->payroll_list_philhealth_er = $allPayrollList[$pl]["payroll_list_philhealth_er"];
-                $payrollList->payroll_list_philhealth_ee = $allPayrollList[$pl]["payroll_list_philhealth_ee"];
-                $payrollList->payroll_list_madatory_ee = $allPayrollList[$pl]["payroll_list_madatory_ee"];
-                $payrollList->payroll_list_sss_loan = $allPayrollList[$pl]["payroll_list_sss_loan"];
-                $payrollList->payroll_list_pagibig_loan = $allPayrollList[$pl]["payroll_list_pagibig_loan"];
-                $payrollList->payroll_list_pagibig_mp2 = $allPayrollList[$pl]["payroll_list_pagibig_mp2"];
-                $payrollList->payroll_list_fca_tuition = $allPayrollList[$pl]["payroll_list_fca_tuition"];
-                $payrollList->payroll_list_fwc_tithes = $allPayrollList[$pl]["payroll_list_fwc_tithes"];
-                $payrollList->payroll_list_other_deduction = $allPayrollList[$pl]["payroll_list_other_deduction"];
-                $payrollList->payroll_list_tax = $allPayrollList[$pl]["payroll_list_tax"];
-                $payrollList->payroll_list_basic_pay = $allPayrollList[$pl]["payroll_list_basic_pay"];
-                $payrollList->payroll_list_gross = $allPayrollList[$pl]["payroll_list_gross"];
-                $payrollList->payroll_list_deduction = $allPayrollList[$pl]["payroll_list_deduction"];
-                $payrollList->payroll_list_net_pay = $allPayrollList[$pl]["payroll_list_net_pay"];
-            }
+            $payrollList->payroll_list_sss_er = $allPayrollList[0]["payroll_list_sss_er"];
+            $payrollList->payroll_list_sss_ee = $allPayrollList[0]["payroll_list_sss_ee"];
+            $payrollList->payroll_list_pagibig_er = $allPayrollList[0]["payroll_list_pagibig_er"];
+            $payrollList->payroll_list_pagibig_ee = $allPayrollList[0]["payroll_list_pagibig_ee"];
+            $payrollList->payroll_list_philhealth_er = $allPayrollList[0]["payroll_list_philhealth_er"];
+            $payrollList->payroll_list_philhealth_ee = $allPayrollList[0]["payroll_list_philhealth_ee"];
+            $payrollList->payroll_list_madatory_ee = $allPayrollList[0]["payroll_list_madatory_ee"];
+            $payrollList->payroll_list_sss_loan = $allPayrollList[0]["payroll_list_sss_loan"];
+            $payrollList->payroll_list_pagibig_loan = $allPayrollList[0]["payroll_list_pagibig_loan"];
+            $payrollList->payroll_list_pagibig_mp2 = $allPayrollList[0]["payroll_list_pagibig_mp2"];
+            $payrollList->payroll_list_fca_tuition = $allPayrollList[0]["payroll_list_fca_tuition"];
+            $payrollList->payroll_list_fwc_tithes = $allPayrollList[0]["payroll_list_fwc_tithes"];
+            $payrollList->payroll_list_other_deduction = $allPayrollList[0]["payroll_list_other_deduction"];
+            $payrollList->payroll_list_tax = $allPayrollList[0]["payroll_list_tax"];
+            $payrollList->payroll_list_basic_pay = $allPayrollList[0]["payroll_list_basic_pay"];
+            $payrollList->payroll_list_gross = $allPayrollList[0]["payroll_list_gross"];
+            $payrollList->payroll_list_deduction = $allPayrollList[0]["payroll_list_deduction"];
+            $payrollList->payroll_list_net_pay = $allPayrollList[0]["payroll_list_net_pay"];
             // update
             $query = checkUpdateAmount($payrollList);
             returnSuccess($payrollList, "Payroll List", $query);
